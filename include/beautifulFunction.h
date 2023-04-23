5b2a45d6137b64f7de72a738cf78c54cba8d98e5
MATOO-Dev <fagoldob@gmail.com>
2023-04-23 22:29:27 +0200

    Included Moodle files

---
+++ include/beautifulFunction.h
@@ -0,0 +1,4 @@
+#pragma once
+
+
+template<class S, class T, int var> S f(T x, S y);
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "beautifulFunction.h"
+
+// Hier die Definition der Datentypen einfügen, die Sie für 
+// den Aufruf der Realisation von f benötigen.
+
+int main() {
+	// Hier den Aufruf Ihrer Realisation von f einfügen.
+}
